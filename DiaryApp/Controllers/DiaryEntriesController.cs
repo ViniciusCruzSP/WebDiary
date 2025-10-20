3502616bd23c52234e2ddb94aa5e29a298050b40
Vinícius da Cruz <vinicius.costa@bib.com.br>
2025-10-20 10:16:57 -0300

    feat: Data seeding add

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DiaryApp.Data;
+using DiaryApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace DiaryApp.Controllers
 {
     public class DiaryEntriesController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public DiaryEntriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<DiaryEntry> entries = _db.DiaryEntries.ToList();
+            return View(entries);
         }
     }
 }
