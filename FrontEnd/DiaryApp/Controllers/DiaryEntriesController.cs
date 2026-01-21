a12e8e068e6a6078cf0f87cfbc6b7f4be13dfcea
Vinícius da Cruz <vinicius.costa@bib.com.br>
2026-01-21 13:34:49 -0300

    feat(mvc): add DiaryApp MVC frontend

---
+++ FrontEnd/DiaryApp/Controllers/DiaryEntriesController.cs
@@ -0,0 +1,92 @@
+﻿using DiaryApp.Data;
+using DiaryApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace DiaryApp.Controllers
+{
+    public class DiaryEntriesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public DiaryEntriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Diary()
+        {
+            List<DiaryEntry> entries = _db.DiaryEntries
+                .OrderByDescending(en => en.Created)
+                .ToList();
+            return View(entries);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(DiaryEntry obj)
+        {
+            if(obj != null && obj.Title.Length < 3)
+            {
+                ModelState.AddModelError("Title", "Title too short");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.DiaryEntries.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Diary");
+            }
+            return View(obj);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            DiaryEntry? diaryEntry = _db.DiaryEntries.Find(id);
+
+            return View(diaryEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(DiaryEntry obj)
+        {
+            if (obj != null && obj.Title.Length < 3)
+            {
+                ModelState.AddModelError("Title", "Title too short");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.DiaryEntries.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Diary");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var entry = _db.DiaryEntries.Find(id);
+
+            if (entry == null)
+                return NotFound();
+
+            _db.DiaryEntries.Remove(entry);
+            _db.SaveChanges();
+
+            return RedirectToAction("Diary");
+        }
+
+    }
+}
---
+++ FrontEnd/DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntry
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a title!")]
+        [StringLength(100, MinimumLength = 3,
+            ErrorMessage = "The title must have at least 3 characters!")]
+        public string Title { get; set; }
+        
+        [Required(ErrorMessage = "Please enter a content!")]
+        [StringLength(800, MinimumLength = 3,
+            ErrorMessage = "The content must have at least 3 characters!")]
+        public string Content { get; set; }
+        
+        [Required(ErrorMessage = "Please enter a date!")]
+        public DateTime Created { get; set; }
+    }
+}
