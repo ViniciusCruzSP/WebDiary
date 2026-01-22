08c65f4bdb5bbd572ca946f3cb557ce2bbc7f65c
Vinícius da Cruz <vinicius.costa@bib.com.br>
2026-01-22 15:50:36 -0300

    refactor(mvc): use diary API service to list entries

---
+++ FrontEnd/DiaryApp/Models/DTO/DiaryEntryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DiaryApp.Models.DTO
+{
+    public class DiaryEntryDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
