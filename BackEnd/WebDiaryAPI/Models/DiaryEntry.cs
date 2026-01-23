66f0169cd836e2cf867ab5213449dfe945d61f5f
Vinícius da Cruz <vinicius.costa@bib.com.br>
2026-01-23 10:34:44 -0300

    chore: copy DiaryEntry model from MVC to API

--- BackEnd/WebDiaryAPI/Models/DiaryEntry.cs
+++ BackEnd/WebDiaryAPI/Models/DiaryEntry.cs
@@ -5,11 +5,18 @@ namespace WebDiaryAPI.Models
     public class DiaryEntry
     {
         public int Id { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Please enter a title!")]
+        [StringLength(100, MinimumLength = 3,
+            ErrorMessage = "The title must have at least 3 characters!")]
         public string Title { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Please enter a content!")]
+        [StringLength(800, MinimumLength = 3,
+            ErrorMessage = "The content must have at least 3 characters!")]
         public string Content { get; set; }
-        [Required]
-        public DateTime Created { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Please enter a date!")]
+        public DateTime Created { get; set; }
     }
 }
