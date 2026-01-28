7faaf748b4452c6d3f4154cc4ad2760bdb244eb4
Vinícius da Cruz <vinicius.costa@bib.com.br>
2026-01-28 11:37:56 -0300

    feat(api): configure asp.net core identity services

--- BackEnd/WebDiaryAPI/Program.cs
+++ BackEnd/WebDiaryAPI/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebDiaryAPI.Data;
+using WebDiaryAPI.Models.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DiaryContext")));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DiaryContext")));
+builder.Services
+    .AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
 
 // Add Swagger
 builder.Services.AddEndpointsApiExplorer();
